436832b2873af600174735875f43d01f1d5456b7
liulq <llq@t-chip.com.cn>
2021-08-18 11:31:31 +0800

    firefly: add compile branch & commitnum message to /proc/version

--- init/version.c
+++ init/version.c
@@ -44,7 +44,7 @@ EXPORT_SYMBOL_GPL(init_uts_ns);
 /* FIXED STRINGS! Don't touch! */
 const char linux_banner[] =
 	"Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
-	LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
+	LINUX_COMPILE_HOST ") (" LINUX_BRANCH": "LINUX_COMMIT ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
 
 const char *linux_banner_ptr = linux_banner;
 EXPORT_SYMBOL_GPL(linux_banner_ptr);
@@ -52,6 +52,6 @@ EXPORT_SYMBOL_GPL(linux_banner_ptr);
 const char linux_proc_banner[] =
 	"%s version %s"
 	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
-	" (" LINUX_COMPILER ") %s\n";
+	" ("LINUX_BRANCH ": " LINUX_COMMIT ")" " (" LINUX_COMPILER ") %s\n";
 
 BUILD_SALT;
